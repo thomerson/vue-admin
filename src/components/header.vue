59827f0814bbfea6d64be4696cc869aa1a084dee
thomerson <tanghongsheng123@126.com>
2018-11-10 15:59:52 +0800

    update

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,7 +1,18 @@
 <template>
-    <div class="header">
-
-    </div>
+  <div class="header">
+    <el-dropdown class="head_user">
+      <span class="el-dropdown-link">
+        Admin<i class="el-icon-arrow-down el-icon--right"></i>
+      </span>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item>黄金糕</el-dropdown-item>
+        <el-dropdown-item>狮子头</el-dropdown-item>
+        <el-dropdown-item>螺蛳粉</el-dropdown-item>
+        <el-dropdown-item disabled>双皮奶</el-dropdown-item>
+        <el-dropdown-item divided>蚵仔煎</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+  </div>
 </template>
 
 <script>
@@ -22,5 +33,9 @@ export default {
   width: 100%;
   height: 70px;
   background-color: #202329;
+}
+.head_user{
+  float: right;
+  vertical-align: middle;
 }
 </style>
